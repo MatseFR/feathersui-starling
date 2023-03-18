fdecc0a55ba51365fa03b97a98f411508d43303f
Mathieu Sénidre <matse@skwatt.com>
2023-03-18 15:00:38 +0100

    created TypeUtil to check for object

---
+++ src/feathers/utils/type/TypeUtil.hx
@@ -0,0 +1,31 @@
+package feathers.utils.type;
+
+/**
+ * ...
+ * @author Matse
+ */
+class TypeUtil 
+{
+
+	// TODO : inline when tested
+	static public function isObject(object:Dynamic):Bool
+	{
+		if (Std.isOfType(object, String)) 
+		{
+			return false;
+		}
+		var type:Type.ValueType = Type.typeof(object);
+		switch (type)
+		{
+			case Type.ValueType.TNull, Type.ValueType.TInt, Type.ValueType.TFloat, Type.ValueType.TBool, Type.ValueType.TFunction, Type.ValueType.TUnknown :
+				return false;
+			
+			case Type.ValueType.TObject :
+				return true;
+			
+			default : // ValueType.TClass
+				return true;
+		}
+	}
+	
+}
