6847661c1f76fe9024e78889cf765576289261c8
Mathieu Sénidre <matse@skwatt.com>
2023-03-26 19:44:02 +0200

    added DragAndDrop demo

---
+++ examples/DragAndDrop/src/feathers/examples/dragDrop/DropTarget.hx
@@ -0,0 +1,61 @@
+package feathers.examples.dragDrop;
+
+import feathers.controls.LayoutGroup;
+import feathers.dragDrop.DragData;
+import feathers.dragDrop.DragDropManager;
+import feathers.dragDrop.IDropTarget;
+import feathers.events.DragDropEvent;
+import starling.display.DisplayObject;
+import starling.display.Quad;
+
+class DropTarget extends LayoutGroup implements IDropTarget
+{
+	private static inline var DEFAULT_COLOR:Int = 0x36322e;
+	private static inline var HOVER_COLOR:Int = 0x26221e;
+	
+	public function new(dragFormat:String) 
+	{
+		super();
+		this._dragFormat = dragFormat;
+		this.addEventListener(DragDropEvent.DRAG_ENTER, dragEnterHandler);
+		this.addEventListener(DragDropEvent.DRAG_EXIT, dragExitHandler);
+		this.addEventListener(DragDropEvent.DRAG_DROP, dragDropHandler);
+	}
+	
+	private var _dragFormat:String;
+	private var _backgroundQuad:Quad;
+	
+	override function initialize():Void
+	{
+		this._backgroundQuad = new Quad(1, 1, DEFAULT_COLOR);
+		this.backgroundSkin = this._backgroundQuad;
+	}
+	
+	private function dragEnterHandler(event:DragDropEvent, dragData:DragData):Void
+	{
+		if (!dragData.hasDataForFormat(this._dragFormat))
+		{
+			return;
+		}
+		DragDropManager.acceptDrag(this);
+		this._backgroundQuad.color = HOVER_COLOR;
+	}
+	
+	private function dragExitHandler(event:DragDropEvent, dragData:DragData):Void
+	{
+		this._backgroundQuad.color = DEFAULT_COLOR;
+	}
+	
+	private function dragDropHandler(event:DragDropEvent, dragData:DragData):Void
+	{
+		var droppedObject:DisplayObject = cast dragData.getDataForFormat(this._dragFormat);
+		droppedObject.x = Math.min(Math.max(event.localX - droppedObject.width / 2,
+			0), this.actualWidth - droppedObject.width); //keep within the bounds of the target
+		droppedObject.y = Math.min(Math.max(event.localY - droppedObject.height / 2,
+			0), this.actualHeight - droppedObject.height); //keep within the bounds of the target
+		this.addChild(droppedObject);
+		
+		this._backgroundQuad.color = DEFAULT_COLOR;
+	}
+	
+}
---
+++ examples/DragAndDrop/src/feathers/examples/dragDrop/Main.hx
@@ -0,0 +1,105 @@
+package feathers.examples.dragDrop;
+
+import feathers.controls.Button;
+import feathers.controls.Label;
+import feathers.controls.LayoutGroup;
+import feathers.dragDrop.IDragSource;
+import feathers.dragDrop.IDropTarget;
+import feathers.layout.AnchorLayout;
+import feathers.layout.AnchorLayoutData;
+import feathers.themes.MetalWorksDesktopTheme;
+import openfl.Lib;
+import starling.core.Starling;
+import starling.display.Quad;
+import starling.display.Sprite;
+import starling.events.Event;
+import starling.events.ResizeEvent;
+
+class Main extends LayoutGroup implements IDragSource implements IDropTarget
+{
+	private static inline var DRAG_FORMAT:String = "draggableQuad";
+	
+	public function new() 
+	{
+		//set up the theme right away!
+		new MetalWorksDesktopTheme();
+		//this.addEventListener(Event.ADDED_TO_STAGE, addedToStageHandler);
+		super();
+	}
+	
+	private var _draggableQuad:Quad;
+	private var _dragSource:DragSource;
+	private var _dropTarget:DropTarget;
+	private var _resetButton:Button;
+	
+	private function reset():Void
+	{
+		this._draggableQuad.x = 40;
+		this._draggableQuad.y = 40;
+		this._dragSource.addChild(this._draggableQuad);
+	}
+
+	override function initialize():Void
+	{
+		super.initialize();
+		
+		this.layout = new AnchorLayout();
+		
+		var aData:AnchorLayoutData;
+		
+		var instructions:Label = new Label();
+		instructions.text = "Drag the square from the left container to the right container.";
+		instructions.layoutData = new AnchorLayoutData(40, null, null, null, 0);
+		this.addChild(instructions);
+		
+		this._resetButton = new Button();
+		this._resetButton.label = "Reset";
+		this._resetButton.layoutData = new AnchorLayoutData(null, null, 40, null, 0);
+		this._resetButton.addEventListener(Event.TRIGGERED, resetButton_triggeredHandler);
+		this.addChild(this._resetButton);
+		
+		this._draggableQuad = new Quad(100, 100, 0xff8800);
+		
+		this._dragSource = new DragSource(DRAG_FORMAT);
+		aData = new AnchorLayoutData(40, null, 40, 80);
+		aData.percentWidth = 30;
+		aData.topAnchorDisplayObject = instructions;
+		aData.bottomAnchorDisplayObject = this._resetButton;
+		this._dragSource.layoutData = aData;
+		this.addChild(this._dragSource);
+		
+		this._dropTarget = new DropTarget(DRAG_FORMAT);
+		aData = new AnchorLayoutData(40, 80, 40, null);
+		aData.percentWidth = 30;
+		aData.topAnchorDisplayObject = instructions;
+		aData.bottomAnchorDisplayObject = this._resetButton;
+		this._dropTarget.layoutData = aData;
+		this.addChild(this._dropTarget);
+		
+		this.reset();
+		
+		this.stage.addEventListener(Event.RESIZE, stageResizeHandler);
+	}
+	
+	private function resetButton_triggeredHandler(event:Event):Void
+	{
+		this.reset();
+	}
+	
+	private function stageResizeHandler(evt:ResizeEvent):Void
+	{
+		updateViewPort(evt.width, evt.height);
+	}
+	
+	private function updateViewPort(width:Int, height:Int):Void
+	{
+		var current:Starling = Starling.current;
+		var scale:Float = current.contentScaleFactor;
+		
+		stage.stageWidth = Std.int(width / scale);
+		stage.stageHeight = Std.int(height / scale);
+		
+		current.viewPort.width = width;//stage.stageWidth * scale;
+		current.viewPort.height = height;//stage.stageHeight * scale;
+	}
+}
