aaf40130e6ca0dfdc442063335163f4d1fc5809b
Mathieu Sénidre <matse@skwatt.com>
2023-04-03 17:32:22 +0200

    Top Coat Light Mobile Theme

---
+++ src/feathers/themes/TopCoatLightMobileTheme.hx
@@ -0,0 +1,76 @@
+/*
+Copyright 2012-2021 Bowler Hat LLC. All Rights Reserved., Marcel Piestansky
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+*/
+package feathers.themes;
+import openfl.display.BitmapData;
+import openfl.utils.Assets;
+import starling.textures.ConcreteTexture;
+import starling.textures.Texture;
+import starling.textures.TextureAtlas;
+
+/**
+ * The "Topcoat Light" theme for mobile Feathers apps.
+ *
+ * <p>This version of the theme embeds its assets. To load assets at
+ * runtime, see <code>TopcoatLightMobileThemeWithAssetManager</code> instead.</p>
+ *
+ * @see http://feathersui.com/help/theme-assets.html
+ */
+class TopCoatLightMobileTheme extends BaseTopcoatLightMobileTheme 
+{
+	private static inline var ATLAS_XML:String = "assets/img/topcoat_light_mobile.xml";
+	
+	private static inline var ATLAS_BITMAP:String = "assets/img/topcoat_light_mobile.png";
+	
+	/**
+	 * Constructor.
+	 */
+	public function new() 
+	{
+		super();
+		this.initialize();
+	}
+	
+	override function initialize():Void
+	{
+		this.initializeTextureAtlas();
+		super.initialize();
+	}
+	
+	private function initializeTextureAtlas():Void
+	{
+		var atlasBitmapData:BitmapData = Assets.getBitmapData(ATLAS_BITMAP);
+		var atlasTexture:Texture = Texture.fromBitmapData(atlasBitmapData, false, false, 2);
+		atlasTexture.root.onRestore = onAtlasTextureRestore;
+		atlasBitmapData.dispose();
+		this.atlas = new TextureAtlas(atlasTexture, Xml.parse(Assets.getText(ATLAS_XML)));
+	}
+	
+	private function onAtlasTextureRestore(texture:ConcreteTexture):Void
+	{
+		var atlasBitmapData:BitmapData = Assets.getBitmapData(ATLAS_BITMAP);
+		this.atlas.texture.root.uploadBitmapData(atlasBitmapData);
+		atlasBitmapData.dispose();
+	}
+}
